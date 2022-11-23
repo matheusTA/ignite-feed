796a59570a2e4933692422a3e0a0ccc342cce532
matheusTA <matrajano12@gmail.com>
2022-11-23 02:00:50 -0300

    refactor: add extends in avatar props

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,15 +1,15 @@
+import { ImgHTMLAttributes } from "react";
 import styles from "./styles.module.css";
 
-interface AvatarProps {
-  src: string;
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
 }
 
-const Avatar = ({ src, hasBorder = true }: AvatarProps) => {
+const Avatar = ({ hasBorder = true, ...props }: AvatarProps) => {
   return (
     <img
+      {...props}
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
     />
   );
 };
